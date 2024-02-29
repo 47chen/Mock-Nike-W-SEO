e5ad0732ea91f9830d28685169ea6b3b184a36b6
47chen <x841010@gmail.com>
2024-02-29 17:53:50 -0500

    create components to build structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  SpecialOffers,
+  SuperQuality,
+  Services,
+  Subscribe,
+} from "./components/index.js";
+
 const App = () => {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffers</section>
-      <section className="bg-pale-blue padding">CustomerReview</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+      {/* <Nav /> */}
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+        <input type="text" />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div>Hero123</div>;
+};
+
+export default Hero;
