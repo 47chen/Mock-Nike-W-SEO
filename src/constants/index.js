ac94880ba3da1b695fe9a6fd95fd778d6cf2e981
47chen <x841010@gmail.com>
2024-03-01 23:06:44 -0500

    Services section"

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,6 +13,8 @@ import {
   thumbnailShoe3,
 } from "../assets/images";
 
+import { truckFast, shieldTick, support } from "../assets/icons";
+
 export const navLinks = [
   { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
@@ -61,5 +63,24 @@ export const products = [
     imgURL: shoe7,
     name: "Nike Air Jordan-001",
     price: "$230.20",
+  },
+];
+
+export const services = [
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
   },
 ];
