1464f9a9c9790a4dda0692ebacbd0d739cc67db4
47chen <x841010@gmail.com>
2024-03-03 16:05:56 -0500

    SpecialOffer section Customized button component Check whether it has certain props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red hover:text-black transition-colors">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor} hover:bg-gray-100`
+          : `bg-coral-red text-white border-coral-red hover:text-black transition-colors`
+      } rounded-full`}
+    >
       {label}
       {iconURL && (
         <img
